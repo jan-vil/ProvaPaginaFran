0461a2109cdfe370bd8882c693e0c15bd9f46da5
Jan-Vil <jvilaplan@ies-eugeni.cat>
2024-09-04 09:07:59 +0200

    Pàgina prova almost finished

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,9 +1,10 @@
 export const LINKS = {
-  github: "https://github.com/Ladvace",
-  linkedin: "https://www.linkedin.com/in/gianmarco-cavallo/",
-  medium: "https://ladvace.medium.com/",
+  github: "https://github.com/Fdreyr",
+  linkedin: "https://www.linkedin.com/in/francisco-r-6a231613a/",
+  instagram:"https://www.youtube.com/watch?v=oavMtUWDBTM",
+/*   medium: "https://ladvace.medium.com/",
   discord: "https://discordapp.com/users/163300027618295808",
-  dribble: "https://dribbble.com/Ladvace_Jace",
+  dribble: "https://dribbble.com/Ladvace_Jace", */
 };
 
 export const loaderAnimation = [
